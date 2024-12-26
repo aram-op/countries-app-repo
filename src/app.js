c1a137b5d84c41cf48da7ba7307e26bf56275220
aram-op <aram.tadevosyan@optimumpartners.co>
2024-12-26 13:32:35 +0400

    Implemented main ui

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const pug = require('pug');
-const path = require("path");
+const path = require('path');
+const data = require('../data.json');
 
 const app = express();
+
+function generateSlug(name) {
+    return name.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
+}
 
 app.set("view engine", "pug");
 
 app.set("views", __dirname + "/views");
 
 app.use(express.static(
-    path.join(__dirname + "/public")));
+    path.join(__dirname, "..",  "public")));
+
 
 app.get("/", (req, res) => {
-    res.render("layout");
+    const countries = data.map(c => ({
+        ...c,
+        slug: generateSlug(c.name)
+    }));
+
+    res.render("index", {data : countries});
+});
+
+app.get('/country/:slug', (req, res) => {
+    const slug = req.params.slug;
+    const country = data.find(c => generateSlug(c.name) === slug);
+
+    if (!country) {
+        return res.status(404).send('Country not found');
+    }
+
+    res.render('country-details.pug', { country });
 });
 
 app.listen(3000);
